f4339121c6d09b5bb56d1b56a597b012eeeb2936
violaxanthin <efimochkina.sofi@gmail.com>
2024-08-19 12:45:01 +0300

    updated schedule

--- app.py
+++ app.py
@@ -40,40 +40,66 @@ bot = Bot(bot_token)
 dp = Dispatcher(storage=storage)
 
 # Start function
+from datetime import datetime, timedelta
+from aiogram import Bot, types
+from aiogram.filters import Command
+from aiogram.types import Message
+
 @dp.message(Command(commands=["start"]))
 async def start(message: Message):
     user_id = message.from_user.id
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+    
     await message.answer(
         text = f'Здравствуйте, {user_full_name}! На связи Ele.Mental – бот самомониторинга. \n\n '
-        'Он создан для того, чтобы помочь вам самостоятельно контролировать свое ментальное состояние.\n' 
-'Формат чат-бота – дневник, который вам предлагается заполнять в с помощью коротких ответов на вопросы из клинических опросников и шкал, '
-' результаты которых будут анализироваться с применением алгоритмов искусственного интеллекта, чтобы сформировать картину стабильности состояния.\n '
-'В случае обнаружения признаков изменений в состоянии, вы получите письменную рекомендацию к посещению доктора. \n'
-'Для вашего удобства бот будет присылать вам уведомления с опросниками 2 раза в день - утром и вечером. \n'
-'Данные, введенные в формы будут использованы исключительно в рамках исследования и в соответствии с политикой персональных данных Google: https://policies.google.com/privacy \n'
-'Перед использованием чат-бота, пожалуйста, обратитесь к медицинскому специалисту.'
-    ) 
-# Schedule first reminder (message '1') at 10:00 every 3 days for 5 times   
-    for i in range(5):
-        scheduler.add_job(
-        apsched.send_message_cron_1,
-        trigger='cron',
-        hour=10,
-        start_date=datetime.now() + timedelta(days=3 * i),
-        kwargs={'bot': bot, 'user_id': user_id, 'user_full_name': user_full_name}
-        )
- # Schedule second reminder (message '2') at 19:00 every 3 days for 5 times                
-        scheduler.add_job(
+        'Он создан для того, чтобы помочь вам самостоятельно контролировать свое ментальное состояние.\n'
+        'Формат чат-бота – дневник, который вам предлагается заполнять в с помощью коротких ответов на вопросы из клинических опросников и шкал, '
+        ' результаты которых будут анализироваться с применением алгоритмов искусственного интеллекта, чтобы сформировать картину стабильности состояния.\n '
+        'В случае обнаружения признаков изменений в состоянии, вы получите письменную рекомендацию к посещению доктора. \n'
+        'Для вашего удобства бот будет присылать вам уведомления с опросниками 2 раза в день - утром и вечером. \n'
+        'Данные, введенные в формы будут использованы исключительно в рамках исследования и в соответствии с политикой персональных данных Google: https://policies.google.com/privacy \n'
+        'Перед использованием чат-бота, пожалуйста, обратитесь к медицинскому специалисту.'
+    )
+
+    start_date = datetime.now()
+
+    # Отправка первой анкеты сразу после регистрации
+    await apsched.send_message_cron_1(bot, user_id, user_full_name)
+
+    # Планирование вечерней анкеты в 19:00 того же дня
+    evening_date = start_date.replace(hour=19, minute=0, second=0, microsecond=0)
+    if start_date > evening_date:
+        evening_date += timedelta(days=1)
+    
+    scheduler.add_job(
         apsched.send_message_cron_2,
-        trigger='cron',
-        hour=19,
-        start_date=datetime.now() + timedelta(days=3 * i),
+        trigger='date',
+        run_date=evening_date,
         kwargs={'bot': bot, 'user_id': user_id}
+    )
+
+    # Планирование остальных анкет
+    next_morning = start_date.replace(hour=10, minute=0, second=0, microsecond=0) + timedelta(days=2)
+    for i in range(4):  # 4 дополнительных цикла
+        # Утренняя анкета в 10:00
+        scheduler.add_job(
+            apsched.send_message_cron_1,
+            trigger='date',
+            run_date=next_morning,
+            kwargs={'bot': bot, 'user_id': user_id, 'user_full_name': user_full_name}
         )
 
-    
+        # Вечерняя анкета в 19:00
+        scheduler.add_job(
+            apsched.send_message_cron_2,
+            trigger='date',
+            run_date=next_morning.replace(hour=19),
+            kwargs={'bot': bot, 'user_id': user_id}
+        )
+
+        next_morning += timedelta(days=2)
+
     scheduler.start()
 
 @dp.message(Command(commands=["help"]))
